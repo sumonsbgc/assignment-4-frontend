cacbc22ee0914de5b4b974559474fdf20ebd12b4
Mohammad Sumon <sumonsbgc@gmail.com>
2026-02-12 03:06:53 +0600

    setup auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,11 +1,14 @@
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
-// import config from "./config";
 
 export const auth = createAuthClient({
-	baseURL: typeof window !== "undefined" ? window.location.origin : "",
+	baseURL: typeof window !== "undefined" ? window.location.origin : process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000",
+	basePath: "/api/auth",
 	fetchOptions: {
 		credentials: "include",
+		headers: {
+			"Content-Type": "application/json",
+		},
 	},
 	plugins: [
 		inferAdditionalFields({
