74c51aab8294c0fa59e2f81f90026a7c26725a79
Mohammad Sumon <sumonsbgc@gmail.com>
2026-02-12 23:50:56 +0600

    change auth

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,18 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
+	async rewrites() {
+		return [
+			{
+				source: "/api/auth/:path*",
+				destination: `${process.env.BACKEND_URL}/api/auth/:path*`,
+			},
+			{
+				source: "/api/v1/:path*",
+				destination: `${process.env.BACKEND_URL}/api/v1/:path*`,
+			},
+		];
+	},
 };
 
 export default nextConfig;
